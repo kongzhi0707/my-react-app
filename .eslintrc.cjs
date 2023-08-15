1c18d4d9652c144be24467209321a6cd44df268f
kongzhi <tugenhua0707@qq.com>
2023-08-15 23:23:05 +0800

    update

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "react"
+  ],
+  "rules": {
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  "settings": {
+    "react": {
+      "version": 'detect'
+    }
+  }
+}
